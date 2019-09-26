d8f67bd65d4c2c9dded8a4725f2d5b42fad68404
Real Ghost <alejandro519@gmail.com>
2019-09-26 07:43:12 -0700

    Added bands search functionality for Bands in Town Artists Events API

--- liri.js
+++ liri.js
@@ -3,22 +3,20 @@ require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret
 });
-
-// get user command
 var command = process.argv[2]
 var userSearch = process.argv.slice(3).join(' ');
 console.log("userSearch: ", userSearch)
-// get user query from user
-// if command is look-up song
+
 if (command === "spotify-this-song") {
   searchSpotify(userSearch)
-} else if (command === "lookup-movie") {
+} else if (command === "movie-this") {
   searchOmdb(userSearch)
+} else if (command === "concert-this") {
+  searchBands(userSearch)
 }
 
 function searchSpotify(spotifyQuery) {
@@ -42,38 +40,58 @@ function printTrack(track) {
   var song = track.name
   var spotifyURL = track.external_urls.spotify
   var artist = track.artists[0].name
-  console.log("Song: " + song)
-  console.log("Album: " + albumName)
-  console.log("URL: " + spotifyURL)
   console.log("Artist: " + artist)
+  console.log("Song: " + song)
+  console.log("URL: " + spotifyURL)
+  console.log("Album: " + albumName)
   console.log('\n--------------------------\n')
 }
 
 function searchOmdb(search) {
   axios.get("http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
+    function (response) {
+      console.log("\n-----------------------------\n")
+      console.log("The movie's title is: " + response.data.Title);
+      console.log("The movie was released in: " + response.data.Year);
+      console.log("The IMDB rating is: " + response.data.imdbRating);
+      console.log("The Rotten Tomatoes Rating is: " + response.data.Ratings[1].Value);
+      console.log("The movie was produced in: " + response.data.Country);
+      console.log("The language of the movie is: " + response.data.Language);
+      console.log("The plot of the movie is: " + response.data.Plot);
+      console.log("The actors in the movie are: " + response.data.Actors);
+      console.log("\n-----------------------------\n")
+
     })
-    .catch(function(error) {
+    .catch(function (error) {
       if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
       } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
         console.log(error.request);
       } else {
-        // Something happened in setting up the request that triggered an Error
         console.log("Error", error.message);
       }
       console.log(error.config);
     });
+}
+
+function searchBands(search) {
+  axios.get("https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp").then(
+    function (response) {
+      console.log("\n-----------------------------\n")
+      console.log("Name of the venue: " + response.data[1].venue.name);
+      console.log("Venue location: " + response.data[1].venue.city + ", " + response.data[1].venue.country);
+      console.log("Date of the event: " + response.data[1].datetime);
+      console.log("\n-----------------------------\n")
+    }
+  )
+  .catch(function (error) {
+    if (error.response) {
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
 }
 
 
